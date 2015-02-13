4ace34a0ea4564cac056e27744ba0610327bc36f
Marcus Ottosson <konstruktion@gmail.com>
2015-02-13 14:38:48 +0000

    Simplified service.py

--- pyblish_nuke/service.py
+++ pyblish_nuke/service.py
@@ -4,15 +4,17 @@ from .version import version
 
 import nuke
 
+wrapper = nuke.executeInMainThreadWithResult
+
 
 class NukeService(pyblish_endpoint.service.EndpointService):
     def init(self, *args, **kwargs):
-        return nuke.executeInMainThreadWithResult(
-            super(NukeService, self).init, args, kwargs)
+        orig = super(NukeService, self).init
+        return wrapper(orig, args, kwargs)
 
     def next(self, *args, **kwargs):
-        return nuke.executeInMainThreadWithResult(
-            super(NukeService, self).next, args, kwargs)
+        orig = super(NukeService, self).next
+        return wrapper(orig, args, kwargs)
 
     def versions(self):
         versions = super(NukeService, self).versions()
