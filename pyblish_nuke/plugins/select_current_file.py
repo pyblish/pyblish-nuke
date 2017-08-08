d896c2b6634c571abbbe78c15bf1baf23078d05d
Toke Jepsen <tokejepsen@gmail.com>
2017-08-08 17:44:05 +0200

    Fix module imports and upgrade to new style plugins.

--- pyblish_nuke/plugins/select_current_file.py
+++ pyblish_nuke/plugins/select_current_file.py
@@ -1,23 +1,19 @@
-import os
-
 import pyblish.api
 
-import nuke
 
-
-@pyblish.api.log
-class SelectCurrentFile(pyblish.api.Selector):
+class SelectCurrentFile(pyblish.api.ContextPlugin):
     """Inject the current working file into context"""
 
-    hosts = ['nuke']
-    version = (0, 1, 0)
+    order = pyblish.api.CollectorOrder
+    hosts = ["nuke"]
 
     def process(self, context):
-        """Todo, inject the current working file"""
+        import os
+        import nuke
+
         current_file = nuke.root().name()
 
-        # Maya returns forward-slashes by default
         normalised = os.path.normpath(current_file)
 
-        context.set_data('current_file', value=normalised)
-        context.set_data('currentFile', value=normalised)
+        context.data["current_file"] = normalised
+        context.data["currentFile"] = normalised
